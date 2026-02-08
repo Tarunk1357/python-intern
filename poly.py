7d779e4caebe65b2e84b885dd478a0dfd9509a52
Tarunk1357 <runnertarun377@gmail.com>
2026-02-08 19:26:31 +0530

    push

---
+++ poly.py
@@ -0,0 +1,30 @@
+# Parent class
+class Payment:
+    def pay(self):
+        print("Making a payment")
+
+# Child class GooglePay
+class GooglePay(Payment):
+    def pay(self):
+        print("Payment done using Google Pay")
+
+# Child class PhonePe
+class PhonePe(Payment):
+    def pay(self):
+        print("Payment done using PhonePe")
+
+# Child class CreditCard
+class CreditCard(Payment):
+    def pay(self):
+        print("Payment done using Credit Card")
+
+# Creating objects 
+p1 = Payment()
+p2 = GooglePay()
+p3 = PhonePe()
+p4 = CreditCard()
+
+p1.pay()
+p2.pay()
+p3.pay()
+p4.pay()
